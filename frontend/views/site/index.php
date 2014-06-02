749a18c788f1a2c718ce4691a24bd2bc7d70a744
Tom King <tom@tskmatrix.com>
2014-06-02 14:26:32 -0500

    initial commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @var yii\web\View $this
+ */
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Congratulations!</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Step 1</h2>
+
+                <p>THis is a 15 part tutorial on Beginning Yii 2.0. The object of the tutorial is to flesh out an ecommerce website. 
+                Be sure that you follow the Tutorial on YouTube.</p>
+
+                <p><a class="btn btn-default" href="https://www.youtube.com/playlist?list=PLMyGpiUTm10799F8FfSai3wvlL4DB4qqX" target="_blank">PlayList &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Setp 2</h2>
+
+                <p>If you've gotten this far, you have already seen the first video and are ready for more.<br/>We will be coding ouy site pretty heavily and you will be able
+                to find the code on github as soon as I can get "a round tuit".</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">GitHub &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Step 3</h2>
+
+                <p>Now you may add additional extensions to your site. Be sure that they are for Yii 2.0.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+    </div>
+</div>
