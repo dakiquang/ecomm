f6a5c7645e4a936d1f59f2a5e00cde121629c975
TSKMatrix <tom@tskmatrix.com>
2014-06-10 08:51:06 -0500

    updated 06-09-2014
    Signed-off-by: TSKMatrix <tom@tskmatrix.com>

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -15,18 +15,16 @@ $this->title = 'Beginning Yii 2.0 - ecomm';
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
     <div class="body-content">
-        <!-- start page HTML -->
         <div class="row">
             <div class="col-lg-4">
                 <h2>Step 1</h2>
-               
                 <p>This is a 15 part tutorial on Beginning Yii 2.0. The object of the tutorial is to flesh out an ecommerce website. 
                 Be sure that you follow the Tutorial on YouTube.</p>
 
                 <p><a class="btn btn-default" href="https://www.youtube.com/playlist?list=PLMyGpiUTm10799F8FfSai3wvlL4DB4qqX" target="_blank">PlayList &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Setp 2</h2>
+                <h2>Step 2</h2>
 
                 <p>If you've gotten this far, you have already seen the first video and are ready for more.<br/>
                 We will be coding ouy site pretty heavily and you will find the code on github.</p>
@@ -42,6 +40,5 @@ $this->title = 'Beginning Yii 2.0 - ecomm';
                 
                  <p><a class="btn btn-default" href="https://packagist.org/search/?q=yii2" target="_blank">packagist &raquo;</a></p>
             </div>
-        </div>
     </div> 
 </div>
